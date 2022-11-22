fb676700ee3c9e5b9477ff4b5edad3feb03ba52e
Daeho Cho <cdh981009@gmail.com>
2022-11-23 00:27:02 +0900

    Use integer arithmetic in bucket_uniform2_choose

--- src/crush/mapper.c
+++ src/crush/mapper.c
@@ -118,14 +118,23 @@ static int bucket_uniform_choose(const struct crush_bucket_uniform *bucket,
 static int bucket_uniform2_choose(const struct crush_bucket_uniform2 *bucket,
                                   int x, int r)
 {
-    __u32 i;
-    __u32 h = crush_hash32_3(bucket->h.hash, x, bucket->h.id, r);
-    h = (__u32) ((double) h / (U32_MAX / (double) bucket->h.size));
+    __u32 i, res;
+    __u32 p;
+    __u64 h = (__u64)crush_hash32_3(bucket->h.hash, x, bucket->h.id, r);
+    __u64 rem = (U32_MAX - bucket->h.size + 1) % bucket->h.size;
+    __u64 l = U32_MAX / bucket->h.size + 1;
 
-    if (h % 2 == 0) {
-        i = h / 2;
+    if (h < rem * l) {
+        res = h / l;
     } else {
-        i = bucket->h.size - (h + 1) / 2;
+        p = h - rem * l;
+        res = p / (l - 1) + rem;
+    }
+
+    if (res % 2 == 0) {
+        i = res / 2;
+    } else {
+        i = bucket->h.size - (res + 1) / 2;
     }
 
     return bucket->h.items[i];
