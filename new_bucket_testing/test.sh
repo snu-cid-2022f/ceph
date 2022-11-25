f2169dea056ac0fd58da003f739673ad7481d370
gwuiinkim <gwuiinkim@gmail.com>
2022-11-25 14:05:44 +0900

    test map sript added.

--- new_bucket_testing/test.sh
+++ new_bucket_testing/test.sh
@@ -1,31 +1,31 @@
 mkdir -p maps csvs decompiled_maps
 
 # crushtool binary 위치
-crushtool_dir=../build/bin/crushtool
+crushtool_dir=/home/ceph/build/bin/crushtool
 
 ##### build crushmap
-build_crushmap_failure_domain_osd() { # $1 map_name, $2 num_osds, $3 bucket_type 
+function build_crushmap_failure_domain_osd() { # $1 map_name, $2 num_osds, $3 bucket_type 
   local map_name=${1}
   local num_osds=${2}
   local bucket_type=${3}
   ${crushtool_dir} -o ./maps/${map_name} --build --num_osds ${num_osds} node ${bucket_type} ${num_osds} root ${bucket_type} 0
 }
 
-build_crushmap_failure_domain_node() { # $1 map_name, $2 num_osds, $3 bucket_type 
+function build_crushmap_failure_domain_node() { # $1 map_name, $2 num_osds, $3 bucket_type 
   local map_name=${1}
   local num_osds=${2}
   local bucket_type=${3}
   ${crushtool_dir} -o ./maps/${map_name} --build --num_osds ${num_osds} node ${bucket_type} 10 root ${bucket_type} 0
 }
 
-build_crushmap_failure_domain_rack() { # $1 map_name, $2 num_osds, $3 bucket_type 
+function build_crushmap_failure_domain_rack() { # $1 map_name, $2 num_osds, $3 bucket_type 
   local map_name=${1}
   local num_osds=${2}
   local bucket_type=${3}
   ${crushtool_dir} -o ./maps/${map_name} --build --num_osds ${num_osds} node ${bucket_type} 10 rack ${bucket_type} 10 root ${bucket_type} 0
 }
 
-build_crushmap_failure_domain_datacenter() { # $1 map_name, $2 num_osds, $3 bucket_type 
+function build_crushmap_failure_domain_datacenter() { # $1 map_name, $2 num_osds, $3 bucket_type 
   local map_name=${1}
   local num_osds=${2}
   local bucket_type=${3}
@@ -50,31 +50,33 @@ function build_map() {
     echo "done"
 }
 
-osd_array=("10;consthash" "10;straw2" "20;consthash" "20;straw2")
-node_array=("30;consthash" "30;straw2" "100;consthash" "100;straw2")
-rack_array=("1000;consthash" "1000;straw2" "5000;consthash" "5000;straw2")
-datacenter_array=("1000;consthash" "1000;straw2" "5000;consthash" "5000;straw2")
-build_map "osd" "${osd_array[@]}"
-build_map "node" "${node_array[@]}"
-build_map "datacenter" "${datacenter_array[@]}"
-
 
 
 
 ##### testmap
-# test_crushmap() {
+test_crushmap() { ## $1 num-rep $2 min-x $3 max-x 
+  local num_rep="$1"
+  local min_x="$2"
+  local max_x="$3"
+  shift 3
+  local arr=("$@")
+  echo "testing crush map.." 
+  for el in "${arr[@]}";
+  do
+    local split=(${el//;/ })
+    local num_osds=${split[0]}
+    local bucket_type=${split[1]}
+    local map_name="${num_osds}${bucket_type}"
+    echo "  ${map_name}"
+    mkdir -p ./csvs/${map_name}
+    cd ./csvs/${map_name}
+    ${crushtool_dir} -i ../../maps/${map_name} --test --output-name ${map_name} --output-csv --num-rep ${num_rep} --min-x ${min_x} --max-x ${max_x}
+    cd ../../
+  done
+  echo "done."
 
-# }
+}
 
-# test crushmap
-# test_crushmap() { # map-name, num-rep, min-x, max-x 
-#   ../crushtool -o $1 --test --output-name $1 --output-csv --num-rep $2 --min-x $3 --max-x $4
-# }
-
-# num_rep=10
-# min_x=0
-# max_x=1024
-# build_crushmap "10consthash" ${num_rep} ${min_x} ${max_x}
 
 
 
@@ -103,6 +105,20 @@ decompile_and_print_crushmap() { # $1 map_name
   done
   
 }
+
+
+################ 여기서부터 명령어 실행
+
+osd_array=("10;consthash" "10;straw2" "20;consthash" "20;straw2")
+# node_array=("30;consthash" "30;straw2" "100;consthash" "100;straw2")
+# rack_array=("1000;consthash" "1000;straw2" "5000;consthash" "5000;straw2")
+# datacenter_array=("1000;consthash" "1000;straw2" "5000;consthash" "5000;straw2")
+# build_map "osd" "${osd_array[@]}"
+# build_map "node" "${node_array[@]}"
+# build_map "datacenter" "${datacenter_array[@]}"
+
+test_crushmap "2" "0" "1024" "${osd_array[@]}"
+
 
 # 잘 만들어졌는지 확인용도
 echo "decompling maps.."
